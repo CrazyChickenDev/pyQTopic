8fa1e24bc2d0f8419688388f165a373c02bcf83c
Tapasweni Pathak <tapaswenipathak@gmail.com>
2015-11-02 06:59:35 +0530

    Revert "Minor improvements"

--- qtopic/pyqtopics.py
+++ qtopic/pyqtopics.py
@@ -1,6 +1,7 @@
 import urllib2
 from bs4 import BeautifulSoup
 import feedparser
+import re
 
 
 ####################################################################
@@ -17,10 +18,11 @@ class QTopic:
         raw_data = str(soup.find_all('span', class_="count"))
         soup = BeautifulSoup(raw_data)
         followers = soup.find_all('span')[0].get_text()
-        return {
+        dict = {
             'topic': topic,
             'followers': followers,
         }
+        return dict
 
     @staticmethod
     def get_some_followers(topic):
@@ -30,10 +32,11 @@ class QTopic:
         raw_data = str(soup.find_all('a', class_='user'))
         soup = BeautifulSoup(raw_data)
         name = soup.get_text()
-        return {
+        dict = {
             'name': name,
             'topic': topic,
         }
+        return dict
 
     @staticmethod
     def get_related_topics(topic):
@@ -46,10 +49,11 @@ class QTopic:
         raw_data = str(soup.find_all('span', class_='TopicName'))
         soup = BeautifulSoup(raw_data)
         related_topics = soup.get_text()
-        return {
+        dict = {
             'topic': topic,
             'related_topics': related_topics,
         }
+        return dict
 
     @staticmethod
     def get_best_questions(topic):
@@ -63,11 +67,12 @@ class QTopic:
             links.append(f['entries'][i]['links'][0]['href'])
             title.append(f['entries'][i]['title'])
             published.append(f['entries'][i]['published'])
-        return {
+        dict = {
             'links': links,
             'title': title,
             'published': published
         }
+        return dict
 
     @staticmethod
     def get_top_stories(topic):
@@ -81,11 +86,12 @@ class QTopic:
             links.append(f['entries'][i]['links'][0]['href'])
             title.append(f['entries'][i]['title'])
             published.append(f['entries'][i]['published'])
-        return {
+        dict = {
             'links': links,
             'title': title,
             'published': published
         }
+        return dict
 
     @staticmethod
     def get_open_questions(topic):
@@ -95,7 +101,8 @@ class QTopic:
         raw_data = str(soup.find_all('div', class_='QuestionText'))
         soup = BeautifulSoup(raw_data)
         title = soup.get_text()
-        return {
+        dict = {
             'question_titles': title,
             'topic': topic,
         }
+        return dict
